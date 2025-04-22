bc19bc596b5ec174ee24d6ffbfbb82b4337f179d
Pujan Pokhrel <ppokhrel@uno.edu>
2025-04-22 02:09:23 -0600

    fixing documents

--- rag_backend/main.py
+++ rag_backend/main.py
@@ -14,6 +14,7 @@ import requests
 import os
 from pydantic import BaseModel
 import numpy as np
+from langchain_core.documents import Document
 
 # Add request model
 class AddUrlRequest(BaseModel):
